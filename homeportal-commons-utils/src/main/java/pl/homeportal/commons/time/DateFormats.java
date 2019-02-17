8388b516db2700098ae1e6eb6a785c38f2fdcc4e
gwrazen <gwrazen@gmail.com>
2019-02-17 10:27:49 +0100

    DateFormats

---
+++ homeportal-commons-utils/src/main/java/pl/homeportal/commons/time/DateFormats.java
@@ -0,0 +1,53 @@
+package pl.homeportal.commons.time;
+
+import java.text.SimpleDateFormat;
+
+public class DateFormats
+{
+    public enum Precision
+    {
+        SECOND,MINUTE,DAY;
+    }
+
+    public static final String DATE_WITH_SECOND = "dd-MM-yyyy hh:mm:ss";
+    public static final String DATE_WITH_MINUTE = "dd-MM-yyyy hh:mm";
+    public static final String DATE_WITH_DAY = "dd-MM-yyyy";
+
+    public static SimpleDateFormat sdfWithSecondPrecision()
+    {
+        return sdf(Precision.SECOND);
+    }
+
+    public static SimpleDateFormat sdfWithMinutePrecision()
+    {
+        return sdf(Precision.MINUTE);
+    }
+
+    public static SimpleDateFormat sdfWithDayPrecision()
+    {
+        return sdf(Precision.DAY);
+    }
+
+    public static SimpleDateFormat sdf(Precision precision)
+    {
+        switch (precision)
+        {
+            case SECOND:
+            {
+                return new SimpleDateFormat(DATE_WITH_SECOND);
+            }
+
+            case MINUTE:
+            {
+                return new SimpleDateFormat(DATE_WITH_MINUTE);
+            }
+
+            case DAY:
+            {
+                return new SimpleDateFormat(DATE_WITH_DAY);
+            }
+
+            default: throw new IllegalArgumentException("Precision not found.");
+        }
+    }
+}
