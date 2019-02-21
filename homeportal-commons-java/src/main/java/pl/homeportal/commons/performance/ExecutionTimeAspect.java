c4e6fb9387e1a5a0220bcf69d9b15b2e1d97a7b7
gwrazen <gwrazen@gmail.com>
2019-02-21 19:09:57 +0100

    removed timer

---
+++ homeportal-commons-java/src/main/java/pl/homeportal/commons/performance/ExecutionTimeAspect.java
@@ -0,0 +1,25 @@
+package pl.homeportal.commons.performance;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionTimeAspect
+{
+    private static final Logger LOG = LoggerFactory.getLogger(ExecutionTimeAspect.class.getSimpleName());
+
+    @Around("@annotation(ExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable
+    {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        float executionTime = (System.currentTimeMillis() - start)/1000f;
+        LOG.info(joinPoint.getSignature() + " executed in " + executionTime + "s");
+        return proceed;
+    }
+}
