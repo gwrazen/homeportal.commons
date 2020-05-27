d1afe2eec21840757f84ecb6d7112b97af0eb008
WRAZEN Grzegorz <grzegorz.wrazen@ext.ec.europa.eu>
2020-05-27 12:16:24 +0200

    - version 5.0 and Pageable

--- homeportal-commons-data/src/main/java/pl/homeportal/commons/data/search/Pageable.java
+++ homeportal-commons-data/src/main/java/pl/homeportal/commons/data/search/Pageable.java
@@ -3,6 +3,7 @@ package pl.homeportal.commons.data.search;
 /**
  * Created by Grzegorz Wrażeń on 25-04-2019
  */
+
 public interface Pageable
 {
     int getPageNumber();
---
+++ homeportal-commons-data/src/main/java/pl/homeportal/commons/data/search/PageableImpl.java
@@ -0,0 +1,49 @@
+package pl.homeportal.commons.data.search;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Grzegorz Wrażeń on 27-05-2020
+ */
+
+@Setter
+public class PageableImpl implements Pageable
+{
+    @Getter(AccessLevel.NONE)
+    private int page = 1;
+
+    @Getter(AccessLevel.NONE)
+    private int maxqty = 20;
+
+    @Getter(AccessLevel.NONE)
+    private String sort = "added";
+
+    @Getter(AccessLevel.NONE)
+    private boolean reverse = true;
+
+    @Override
+    public int getPageNumber()
+    {
+        return page;
+    }
+
+    @Override
+    public int getPageSize()
+    {
+        return maxqty;
+    }
+
+    @Override
+    public String getSortField()
+    {
+        return sort;
+    }
+
+    @Override
+    public boolean isReverseOrder()
+    {
+        return reverse;
+    }
+}
