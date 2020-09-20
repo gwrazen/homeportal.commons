cb824354688a7bc72d6147dd96287bbdf392b598
WRAZEN Grzegorz <grzegorz.wrazen@ext.ec.europa.eu>
2020-09-20 10:25:17 +0200

    - added CollectionUtils.java

---
+++ homeportal-commons-java/src/main/java/pl/homeportal/commons/collections/CollectionUtils.java
@@ -0,0 +1,21 @@
+package pl.homeportal.commons.collections;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+
+/**
+ * Created by Grzegorz Wrazen on 20-09-2020
+ */
+
+public class CollectionUtils
+{
+    public static Set<String> asSet(String... parameters)
+    {
+        Set<String> parameterSet = new HashSet<>(parameters.length);
+        parameterSet.addAll(asList(parameters));
+        return parameterSet;
+    }
+}
