e3e2caef4ed17e2b1d42baad0d4d980ebbe42172
WRAZEN Grzegorz <grzegorz.wrazen@homeportal.pl>
2021-05-14 13:47:42 +0200

    -- LanguageResolver

---
+++ homeportal-commons-java/src/main/java/pl/homeportal/commons/i18n/LanguageResolver.java
@@ -0,0 +1,38 @@
+package pl.homeportal.commons.i18n;
+
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
+import static org.springframework.web.servlet.DispatcherServlet.LOCALE_RESOLVER_ATTRIBUTE;
+
+/**
+ * Created by Grzegorz Wrazen on 19-02-2014 at 12:31
+ */
+
+public class LanguageResolver
+{
+    public static Language resolveLanguage(HttpServletRequest request)
+    {
+        LocaleResolver localeResolver = (LocaleResolver) request.getAttribute(LOCALE_RESOLVER_ATTRIBUTE);
+        if (localeResolver != null)
+        {
+            Locale locale = localeResolver.resolveLocale(request);
+            return Language.getByValue(locale.getLanguage());
+        }
+
+        return null;
+    }
+
+    public static Locale resolveLocale(HttpServletRequest request)
+    {
+        LocaleResolver localeResolver = (LocaleResolver) request.getAttribute(LOCALE_RESOLVER_ATTRIBUTE);
+        if (localeResolver != null)
+        {
+            return localeResolver.resolveLocale(request);
+        }
+
+        return null;
+    }
+}
