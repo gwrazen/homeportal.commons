62244a8d9b91af3e1ee116bce42917afc892e498
WRAZEN Grzegorz <grzegorz.wrazen@homeportal.pl>
2024-08-17 12:24:37 +0200

    -- refactoring

--- homeportal-commons-java/src/main/java/pl/homeportal/commons/file/Files.java
+++ homeportal-commons-java/src/main/java/pl/homeportal/commons/file/Files.java
@@ -1,5 +1,7 @@
 package pl.homeportal.commons.file;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
 import java.util.regex.Pattern;
 
@@ -7,10 +9,19 @@ import java.util.regex.Pattern;
  * Created by Grzegorz Wrażeń on 16-08-2024 at 16:49
  */
 
+@Slf4j
 public class Files
 {
+    public static final String ERROR_DELETING_FILE = "Error deleting file: %s";
+    public static final String DELETED_FILE = "Deleted file: %s";
+
     public static boolean deleteDirectory(File directory)
     {
+        if (directory == null)
+        {
+            return false;
+        }
+
         try
         {
             final File[] files = directory.listFiles();
@@ -21,6 +32,7 @@ public class Files
                     if (file.isFile())
                     {
                         file.delete();
+                        log.info(DELETED_FILE, file.getName());
                         continue;
                     }
                     deleteDirectory(file);
@@ -30,13 +42,18 @@ public class Files
         }
         catch (Exception e)
         {
-            e.printStackTrace();
+            log.error(ERROR_DELETING_FILE, directory.getAbsolutePath());
             return false;
         }
     }
 
     public static void deleteFiles(File directory, Pattern filePattern)
     {
+        if (directory == null || filePattern == null)
+        {
+            return;
+        }
+
         try
         {
             final File[] files = directory.listFiles();
@@ -47,15 +64,19 @@ public class Files
                     if (filePattern.matcher(file.getName()).matches())
                     {
                         file.delete();
+                        log.info(DELETED_FILE, file.getName());
                         continue;
                     }
-                    deleteDirectory(file);
+                    if (file.isDirectory())
+                    {
+                        deleteFiles(file, filePattern);
+                    }
                 }
             }
         }
         catch (Exception e)
         {
-            e.printStackTrace();
+            log.error(ERROR_DELETING_FILE, directory.getAbsolutePath(), e);
         }
     }
 }
