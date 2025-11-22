0dbbd0d2b11f6a5bdbccaf33d0a3bbf832f15a73
WRAZEN Grzegorz <grzegorz.wrazen@homeportal.pl>
2025-11-22 11:21:29 +0100

    -- refactoring

---
+++ homeportal-commons-data/src/main/java/pl/homeportal/commons/data/model/Market.java
@@ -0,0 +1,10 @@
+package pl.homeportal.commons.data.model;
+
+/**
+ * Created by Grzegorz Wrażeń on 22/11/2025 at 09:24
+ */
+public class Market
+{
+    public static final String PRIMARY   = "1";
+    public static final String SECONDARY = "2";
+}
