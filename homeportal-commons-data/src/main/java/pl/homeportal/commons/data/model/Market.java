ea1c6db3120b8a79d2e94b25e340ea485a48c132
WRAZEN Grzegorz <grzegorz.wrazen@homeportal.pl>
2025-11-22 13:23:30 +0100

    -- refactoring

--- homeportal-commons-data/src/main/java/pl/homeportal/commons/data/model/Market.java
+++ homeportal-commons-data/src/main/java/pl/homeportal/commons/data/model/Market.java
@@ -7,4 +7,30 @@ public class Market
 {
     public static final String PRIMARY   = "1";
     public static final String SECONDARY = "2";
+
+    public static final String PRIMARY_STRING   = "pierwotny";
+    public static final String SECONDARY_STRING = "wtórny";
+
+    public static final boolean isPrimary(String marketAsString)
+    {
+        return PRIMARY_STRING.equalsIgnoreCase(marketAsString);
+    }
+
+    public static final boolean isSecondary(String marketAsString)
+    {
+        return SECONDARY_STRING.equalsIgnoreCase(marketAsString);
+    }
+
+    public String asId(String marketAsString)
+    {
+        if (isPrimary(marketAsString))
+        {
+            return PRIMARY;
+        }
+        if (isSecondary(marketAsString))
+        {
+            return SECONDARY;
+        }
+        return null;
+    }
 }
